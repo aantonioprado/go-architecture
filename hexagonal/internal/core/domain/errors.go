44adffce51ddac7ea2e65b13049db13ad9a4da4a
aantonioprado <antonioeliaspr@gmail.com>
2026-09-21 10:07:30 -0300

    feat(hexagonal): add user domain entity

---
+++ hexagonal/internal/core/domain/errors.go
@@ -0,0 +1,10 @@
+package domain
+
+import "errors"
+
+var (
+	ErrNameRequired  = errors.New("name is required")
+	ErrEmailRequired = errors.New("email is required")
+	ErrEmailTaken    = errors.New("email already in use")
+	ErrUserNotFound  = errors.New("user not found")
+)
